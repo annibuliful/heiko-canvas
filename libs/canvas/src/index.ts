157818d90c8723126724f93b4e86b421aea1dcc3
lagmanzaza <lagmanmc@gmail.com>
2022-07-10 16:40:18 +0700

    chore: log

--- libs/canvas/src/index.ts
+++ libs/canvas/src/index.ts
@@ -5,7 +5,7 @@ export { Point } from './shape/point';
 export { CanvasObject } from './shape/object';
 export { CanvasRectangle } from './shape/rectangle';
 export { CanvasCircle } from './shape/circle';
-
+export { CanvasTriangle } from './shape/triangle';
 // utils
 export {
   colorNameToHex,
